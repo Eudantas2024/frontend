7431e9aa7e9675328b6a56f64866798b29a02dce
Eudantas <eudantas.alves@gmail.com>
2025-05-20 15:45:04 -0300

    integrador frontend

--- js/moderador.js
+++ js/moderador.js
@@ -33,6 +33,9 @@ function carregarReclamacoes() {
                 <p><strong>Registrado em:</strong> ${new Date(rec.data).toLocaleString()}</p>
                 <p><strong>Empresa Reclamada:</strong> ${rec.empresa}</p>
                 <p><strong>Nome:</strong> ${rec.nome} | <strong>Email:</strong> ${rec.email}</p>
+                <strong>Endereço:</strong> ${rec.logradouro}</p> | <strong>Nº:</strong> ${rec.numero}</p> 
+                | <strong>Bairro:</strong> ${rec.bairro}</p> | <strong>Complemento:</strong> ${rec.complemento}</p> |
+                <strong>Email:</strong> ${rec.cidade}</p> | <strong>UF:</strong> ${rec.uf}</p>
                 <p><strong>Comentário:</strong> ${rec.comentario}</p>
                 <button class="btn-excluir" data-id="${rec._id}">Excluir</button>
                 <button class="editar-btn" data-id="${rec._id}">Editar</button>
