b4d93ed323500c4ea9549d29f0c8d3a647d940f3
Eudantas <eudantas.alves@gmail.com>
2025-05-20 16:03:53 -0300

    integrador frontend

--- js/publico.js
+++ js/publico.js
@@ -7,30 +7,27 @@ function carregarReclamacoes() {
     .then((reclamacoes) => {
       const container = document.getElementById("listaReclamacoes");
       container.innerHTML = "";
-      reclamacoes
-        .slice()
-        .reverse()
-        .forEach((rec) => {
-          const data = new Date(rec.data);
-          const dataFormatada = data.toLocaleDateString("pt-BR");
-          const horaFormatada = data.toLocaleTimeString("pt-BR", {
-            hour: "2-digit",
-            minute: "2-digit",
-          });
-
-          const div = document.createElement("div");
-          div.className = "reclamacao";
-          div.innerHTML = `
-            <br>
-              <span3> ${rec.empresa}</span3>
-              <p><strong>Cliente </strong><span>${rec.nome}</span></p>
-            <p><span2> ${dataFormatada}, ${rec.cidade} - ${rec.uf}</span2>.</p><br>
-            <p><strong>Opinião:</strong> <br>${rec.comentario}</p>
-            <br>
-            <hr/>
-          `;
-          container.appendChild(div);
+      reclamacoes.forEach((rec) => {
+        const data = new Date(rec.data);
+        const dataFormatada = data.toLocaleDateString("pt-BR");
+        const horaFormatada = data.toLocaleTimeString("pt-BR", {
+          hour: "2-digit",
+          minute: "2-digit",
         });
+
+        const div = document.createElement("div");
+        div.className = "reclamacao";
+        div.innerHTML = `
+          <br>
+            <span3> ${rec.empresa}</span3>
+            <p><strong>Cliente </strong><span>${rec.nome}</span></p>
+          <p><span2> ${dataFormatada}, ${rec.cidade} - ${rec.uf}</span2>.</p><br>
+          <p><strong>Opinião:</strong> <br>${rec.comentario}</p>
+          <br>
+          <hr/>
+        `;
+        container.appendChild(div);
+      });
     })
     .catch(() => {
       document.getElementById("listaReclamacoes").innerText =
